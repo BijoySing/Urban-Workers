d86d95bed6ce0df29bf42c8edcc3ffd50e9be796
BijoySing <bijoysingh573@gmail.com>
2023-08-15 03:17:48 +0600

    all

--- php/signupWorker.php
+++ php/signupWorker.php
@@ -1,15 +1,22 @@
 <?php
- $FullName =filter_input(INPUT_POST,'full_name');
- $Email =filter_input(INPUT_POST,'email');
- if(!empty($name))
- {
+
+ $full_name =filter_input(INPUT_POST,'full_name');
+ $email =filter_input(INPUT_POST,'email');
+ $address =filter_input(INPUT_POST,'address');
+ $city =filter_input(INPUT_POST,'city');
+ $contact =filter_input(INPUT_POST,'contact');
+ $service =filter_input(INPUT_POST,'service');
+ $pass =filter_input(INPUT_POST,'pass');
+ $confirm =filter_input(INPUT_POST,'confirm');
+//  if(!empty($name))
+//  {
     $host = "localhost";
-    $dbname = "test";
-    $dbpass = "";
-    $dbuname = "root";
+    $dbname = "urban-workers";
+    $dbpass = "admin";
+    $dbuname = "admin";
     $conn = new mysqli($host,$dbuname,$dbpass,$dbname);
    
-        $sql = "INSERT INTO nihan (name,job) values('$name','$job')";
+        $sql = "INSERT INTO signupworkers (full_name,email,address,city,contact,service,pass,confirm) values('$full_name','$email','$address','$city','$contact','$service','$pass','$confirm')";
         if($conn->query($sql)){
             echo "new record inserted successfully ";
         }
@@ -21,4 +28,4 @@
     }
  
  
- ?>
+ ?>``````````````````````````````````````````````````````````                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
